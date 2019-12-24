c4472d076526ee1da1449fd8a86aac8a5defbcc7
Anil Nair <aniln@vmware.com>
2019-12-24 23:33:04 +0530

    Model for signup

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,40 @@
-var modal = document.getElementById("myModal");
+var signUpmodal = document.getElementById("signUpModal");
+var signInmodal = document.getElementById("signInModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("signUp");
+var signUpBtn = document.getElementById("signUp");
+var signInBtn = document.getElementById("signIn");
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var signUpSpan = document.getElementsByClassName("close")[0];
+var signInSpan = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+signUpBtn.onclick = function() {
+  signUpmodal.style.display = "block";
+}
+signInBtn.onclick = function() {
+  signInmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+signUpSpan.onclick = function() {
+  signUpmodal.style.display = "none";
 }
+
+signInSpan.onclick = function() {
+  signInmodal.style.display = "none";
+}
+
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == signUpmodal) {
+    signUpmodal.style.display = "none";
+  }
+  if (event.target == signInmodal) {
+    signInmodal.style.display = "none";
   }
 }
