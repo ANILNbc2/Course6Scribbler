4fb6db60be3f3617d5d47f5172d30db408d5cd54
Anil Nair <aniln@vmware.com>
2019-12-25 15:23:13 +0530

    add JS for postlists

---
+++ scripts/postslist.js
@@ -0,0 +1,30 @@
+// var trashBtn = document.getElementsByClassName("trash-btn");
+var trashBtn1 = document.getElementById("firstPost");
+var trashBtn2 = document.getElementById("secondPost");
+var trashBtn3 = document.getElementById("thirdPost");
+var trashBtn4 = document.getElementById("forthPost");
+var trashBtn5 = document.getElementById("fifthPost");
+
+
+var deleteModal = document.getElementById("deleteModal");
+
+
+trashBtn1.onclick = function() {
+	deleteModal.style.display = "block";
+}
+
+trashBtn2.onclick = function() {
+	deleteModal.style.display = "block";
+}
+
+trashBtn3.onclick = function() {
+	deleteModal.style.display = "block";
+}
+
+trashBtn4.onclick = function() {
+	deleteModal.style.display = "block";
+}
+
+trashBtn5.onclick = function() {
+	deleteModal.style.display = "block";
+}
