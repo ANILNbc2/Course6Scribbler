d997dcf35e09b5ef71d3120945bcabb23b2d6537
Anil Nair <aniln@vmware.com>
2019-12-27 11:23:47 +0530

    cleanup postslist

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -130,15 +130,6 @@ function postClickShowCard(){
 	}	
 }
 
-// for(var i=0;i<length;i++){	
-// 	trashBtn[i].onclick = function(){
-// 		alert(i);
-// 		var delM = deleteModal[i].id;
-// 		alert(delM);
-// 		deleteModal[i].style.display = "block";	
-// 	}
-// }
-
 function onClickFun(index){
 	var delTemplate = '<div id="deleteModal">' +
 	        '<div class="delete-modal-content">' +
@@ -170,52 +161,3 @@ function noClicked(){
 	deleteModal.style.display = "none";
 	postClickShowCard();
 }
-
-
-
-// var trashBtn1 = document.getElementById("firstPost");
-// var trashBtn2 = document.getElementById("secondPost");
-// var trashBtn3 = document.getElementById("thirdPost");
-// var trashBtn4 = document.getElementById("forthPost");
-// var trashBtn5 = document.getElementById("fifthPost");
-
-
-// // var deleteModal = document.getElementById("deleteModal");
-
-
-// trashBtn1.onclick = function() {
-// 	deleteModal.style.display = "block";
-// }
-
-// trashBtn2.onclick = function() {
-// 	deleteModal.style.display = "block";
-// }
-
-// trashBtn3.onclick = function() {
-// 	deleteModal.style.display = "block";
-// }
-
-// trashBtn4.onclick = function() {
-// 	deleteModal.style.display = "block";
-// }
-
-// trashBtn5.onclick = function() {
-// 	deleteModal.style.display = "block";
-// }
-
-
-// var trashBtnArr = [];
-// var trash-btn = document.getElementsByClassName("trash-btn");
-// var deleteModal = document.getElementById("deleteModal");
-
-// alert("size of trash-btn array = " + trash-btn.length);
-
-
-// for(var i=0;i<trash-btn.length;i++){
-// 	trashBtnArr.push(trash-btn[i]);
-// 	trashBtnArr[i].onclick = function(){
-// 		deleteModal.style.display = "block";
-// 	}
-// }
-
-
